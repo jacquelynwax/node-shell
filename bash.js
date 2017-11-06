ba053baa6a524798e0649c8cf83b9aba85696806
Erika Weil <erikaweil@outlook.com>
2017-11-06 11:01:08 -0500

    working pwd

---
+++ bash.js
@@ -0,0 +1,19 @@
+// console.log(Object.keys(process));
+
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+
+  if (cmd === "pwd") {
+    var pwd = process.argv[1];
+    process.stdout.write(pwd);
+    process.stdout.write('\nprompt > ');
+  }
+
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt > ');
+
+});
