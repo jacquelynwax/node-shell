e6d097b7dd8975a20ba666245ed04ee5086b90d8
Jacquelyn Wax <jacquelynwax@gmail.com>
2017-11-06 12:09:15 -0500

    finished ls

--- bash.js
+++ bash.js
@@ -8,15 +8,13 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-   if (cmd === "pwd") {
-       commands.pwd();
-   }
-   if (cmd === "date") {
-    var date = new Date();
-    process.stdout.write(date.toString());
-  }
-  console.log('TEST')
-  
-  process.stdout.write('\nprompt > ');
+    if (cmd === "pwd") {
+      commands.pwd();
+    } else if (cmd === "date") {
+      var date = new Date();
+      process.stdout.write(date.toString());
+      process.stdout.write('\nprompt > ');
+    } else if (cmd === "ls") {
+      commands.ls();
+    }
 });
-
--- commands.js
+++ commands.js
@@ -1,9 +1,24 @@
-
 var pwd = function() {
     var pwd = process.argv[1];
     process.stdout.write(pwd);
+    process.stdout.write('\nprompt > ');
 }
 
-  
-module.exports = {pwd}
+var fs = require("fs");
 
+var ls = function() {
+  fs.readdir(".", function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      })
+      process.stdout.write("prompt > ");
+    });
+}
+
+
+
+
+
+
+module.exports = {pwd, ls}
